f94808f55bd6fc9f2d3cadcd42a03d35242aec20
yuma.nagaoka <yuma.nagaoka@luxiar.com>
2022-02-09 13:56:26 +0900

    createアクションの位置を移動

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,6 +15,12 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
   def update
     task = Task.find(params[:id])
     task.update!(task_params)
@@ -25,12 +31,6 @@ class TasksController < ApplicationController
     task = Task.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
-  end
-
-  def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
 
   private
