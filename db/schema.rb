b9695e5b379ff6b46cf9f31261404db309fc82ae
yuma.nagaoka <yuma.nagaoka@luxiar.com>
2022-02-10 12:02:56 +0900

    4-5-4-1 Userモデルにadminフラグを追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_022028) do
+ActiveRecord::Schema.define(version: 2022_02_10_025830) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_02_10_022028) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
   end
 
 end
