b4bbd214c3ebfb64cf6d82791f3e5cb8c175a9b9
yuma.nagaoka <yuma.nagaoka@luxiar.com>
2022-02-10 13:28:32 +0900

    4-5-4-2 users_controllerのtypo修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,7 +7,7 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to_admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました。"
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました。"
     else
       render :new
     end
