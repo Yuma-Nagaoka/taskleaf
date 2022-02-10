271409737dc7b6670ef13fc4f0d42f669e42790c
yuma.nagaoka <yuma.nagaoka@luxiar.com>
2022-02-10 11:34:07 +0900

    4-5-2 Userモデルを作る

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_062200) do
+ActiveRecord::Schema.define(version: 2022_02_10_022028) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
