11ffeb2dbfcc4ccf2e670d105cff351ebf2d3ba7
yuma.nagaoka <yuma.nagaoka@luxiar.com>
2022-02-14 15:39:20 +0900

    4-7 タスク一覧を作成日時の新しい順に表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
@@ -19,7 +19,7 @@ class TasksController < ApplicationController
     @task = current_user.tasks.new(task_params)
 
     if @task.save
-      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+      redirect_to @task , notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
     end
